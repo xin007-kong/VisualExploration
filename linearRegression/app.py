1c5de40e142dba24cbc8a60d1d79dde159784457
xin007-kong <1325697914@qq.com>
2023-05-09 07:12:22 +0800

    添加线性回归前后端

---
+++ linearRegression/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from model import LinearRegressor
+
+app = Flask(__name__)
+
+model = LinearRegressor()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    x_new = float(request.form["x_new"])
+    y_new = model.predict(np.array([[x_new]]))
+    return render_template("predict.html", x_new=x_new, y_new=y_new)
+
+
+if __name__ == "__main__":
+    # Load the data and train the model
+    X = np.array([1, 2, 3, 4, 5]).reshape((-1, 1))
+    y = np.array([2, 4, 6, 8, 10])
+    model.fit(X, y)
+
+    # Run the application
+    app.run(debug=True)
---
+++ linearRegression/model.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+class LinearRegressor:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
